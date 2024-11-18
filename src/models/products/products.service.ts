5cb58ba0d354f44157b8fcd06c340afdfe423937
InnaSysoeva <inna.sysoeva24@gmail.com>
2024-11-18 18:07:41 +0200

    Task 20/2: BE: Implement price filtering

--- src/models/products/products.service.ts
+++ src/models/products/products.service.ts
@@ -30,13 +30,46 @@ export class ProductsService {
   ): Promise<ProductInterface[]> {
     const skip = (page - 1) * limit;
     const query = buildFilterQuery(filters);
+    let products: ProductInterface[]
 
     try {
-      const products = await this.productModel
-        .find(query)
-        .skip(skip)
-        .limit(limit)
-        .exec();
+      if(query.price) {
+        delete query.price;
+
+        products = await this.productModel.aggregate([
+          {
+            $addFields: {
+              discountedPrice: {
+                $multiply: [
+                  "$price",
+                  {$subtract: [1, {$divide: ["$discount", 100]}]}
+                ]
+              }
+            }
+          },
+          {
+            $match: {
+              $and: [
+                query,
+                {
+                  discountedPrice: {
+                    $gte: Number(filters.minPrice),
+                    $lte: Number(filters.maxPrice)
+                  }
+                }
+              ]
+            }
+        },
+        {$skip: skip},
+        {$limit: Number(limit)} 
+        ])
+      } else {
+        products = await this.productModel
+         .find(query)
+         .skip(skip)
+         .limit(limit)
+         .exec();
+      }
 
       return products;
     } catch (error) {
